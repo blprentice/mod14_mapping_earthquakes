09e4c7156617076a9618db502f041853617d4477
Benjamin Prentice <benjamin.prentice@outlook.com>
2023-04-15 11:23:38 -0600

    updated logicStep5.js

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -21,6 +21,15 @@ let baseMaps = {
   "Satellite": satelliteStreets,
 };
 
+// Create the earthquake layer for our map.
+let _earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: _earthquakes
+};
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [39.5, -98.5],
@@ -28,20 +37,10 @@ let map = L.map('mapid', {
   layers: [streets]
 });
 
-// Create the earthquake layer for our map.
-let earthquakes = new L.layerGroup();
-
-// We define an object that contains the overlays.
-// This overlay will be visible all the time.
-let overlays = {
-  Earthquakes: earthquakes
-};
-
-// Then we add a control to the map that will allow the user to change
-// which layers are visible.
+// Then we add a control to the map that will allow the user to change which layers are visible.
 L.control.layers(baseMaps, overlays).addTo(map);
 
-// Accessing the Past 7 days earthquake data via GeoJSON URL.
+// Accessing last 7 days of earthquakes GeoJSON URL.
 let earthquakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // This function returns the style data for each of the earthquakes we plot on
@@ -105,36 +104,42 @@ d3.json(earthquakes).then(function(data) {
     onEachFeature: function(feature, layer) {
       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
     }
-  }).addTo(earthquakes);
+  }).addTo(_earthquakes);
 
-  // Then we add the earthquake layer to our map
-  earthquakes.addTo(map);
+  // Then we add the earthquake layer to our map.
+  _earthquakes.addTo(map);
 
-// Then we add a legend to the map.
-  let legend = L.control({position: 'bottomright'});
+  // Create a legend control object.
+  let legend = L.control({
+    position: "bottomright"
+  });
 
-legend.onAdd = function () {
-  let div = L.DomUtil.create("div", "info legend");
+  // Then add all the details for the legend.
+  legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+  };
+  
   const magnitudes = [0, 1, 2, 3, 4, 5];
-const colors = [
-  "#98ee00",
-  "#d4ee00",
-  "#eecc00",
-  "#ee9c00",
-  "#ea822c",
-  "#ea2c2c"
-];
-};
+  
+  const colors = [
+        "#98ee00",
+        "#d4ee00",
+        "#eecc00",
+        "#ee9c00",
+        "#ea822c",
+        "#ea2c2c"
+  ];
 
-// Looping through our intervals to generate a label with a colored square for each interval.
-for (var i = 0; i < magnitudes.length; i++) {
-  console.log(colors[i]);
-  div.innerHTML +=
-    "<i style='background: " + colors[i] + "'></i> " +
-    magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
-}
-return div;
-};
+  // Looping through our intervals to generate a label with a colored square for each interval.
+  for (var i = 0; i < magnitudes.length; i++) {
+          
+    console.log(colors[i]);
 
-legend.addTo(map);
-});
+    div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " + magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+      }
+
+      return div;
+  });
+
+  legend.addTo(map);
