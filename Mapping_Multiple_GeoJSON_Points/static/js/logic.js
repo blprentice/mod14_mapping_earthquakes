7b90b3dcc2d13e17f48e2abcf1e459d5d6441dce
Benjamin Prentice <benjamin.prentice@outlook.com>
2023-04-14 21:24:13 -0600

    updated logic.js file

--- Mapping_Multiple_GeoJSON_Points/static/js/logic.js
+++ Mapping_Multiple_GeoJSON_Points/static/js/logic.js
@@ -1,9 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v12/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -11,8 +8,28 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// Then we add our 'streets' tile layer to the map.
-streets.addTo(map);
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/blprentice/mod14_mapping_earthquakes/Mapping_Multiple_GeoJSON_Points/Mapping_Multiple_GeoJSON_Points/majorAirports.json";
@@ -21,10 +38,12 @@ let airportData = "https://raw.githubusercontent.com/blprentice/mod14_mapping_ea
 d3.json(airportData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-    onEachFeature: function(feature, layer) {
-        console.log(layer);
-        layer.bindPopup("<h3> Airport code: " + feature.properties.faa + "<h2> <hr> </h2>" + "<h3> Airport Name: </h3>" + feature.properties.name + "</h3>")
-    }
-  }).addTo(map);
-});
+  L.geoJson(data).addTo(map);
+  });
+//   L.geoJSON(data, {
+//     onEachFeature: function(feature, layer) {
+//         console.log(layer);
+//         layer.bindPopup("<h3> Airport code: " + feature.properties.faa + "<h2> <hr> </h2>" + "<h3> Airport Name: </h3>" + feature.properties.name + "</h3>")
+//     }
+//   }).addTo(map);
+// });
